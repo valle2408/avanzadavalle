6e00529cf9c0e78d3bf18a2196517ccbac5e34d2
Valle <vallegarciajuan8@gmail.com>
2026-06-02 19:49:41 -0400

    funcion del dashboard , verifica que el usuario haya iniciado sesion y muestra los roles y los modulos segun el rol y manda al dashboard

--- principal/views.py
+++ principal/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required ## llamamos con decoradores
+from usuarios.models import UsuarioRol
 
-# Create your views here.
+#inicio de sesion requerida para esto
+@login_required 
+
+
+def dashboard(request):
+
+
+    roles_usuario = UsuarioRol.objects.filter(usuario=request.user)
+    permisos = {
+        'registrar_compras_cafe': 0,
+        'historial_compras': 0,
+        'comunidades': 0,
+        'productores': 0,
+        'registro_ventas_cafe': 0,
+        'analisis_predicciones': 0,
+        'roles_usuarios': 0,
+    }
+    
+    #recorremos todos los roles que tiene el usuario
+    for usuario_rol in roles_usuario:
+
+        #obtenemos el rol que tiene
+        rol = usuario_rol.role
+
+        # recorremos los permisos por modulos 
+        for modulo in permisos.keys():
+
+            #obtenemos el permiso
+            permiso_actual = getattr(rol,modulo)
+
+            #si el rol es mayor al permiso lo reemplazamos
+            if permiso_actual > permisos[modulo]:
+                permisos[modulo] = permiso_actual
+    
+    #dato que enviamos al html de dashboard
+    context = {
+        'usuario': request.user,
+        'permisos': permisos,
+        'roles': [ur.usuario_rol.rol.nombre_rol for ur in roles_usuario],
+        
+    }
+    return render(request, 'principal/dashboard.html', context)
