b03359671c05a30cd3e4a8efad480fca4753d371
Valle <vallegarciajuan8@gmail.com>
2026-06-02 19:16:06 -0400

    realizamos la logica de inicio  y cierre de sesion en views.py en la carpeta usuarios

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+from .forms import FormularioLogin # DE LA CARPETA QUE CREAMOS IMPORTAMOS SU CLASE
+from .models import UsuarioRol, Rol
+#FUNCION DEL LOGIN
+def iniciar_sesion(request):
+
+    # si el usuario ya inicio sesion se manda directamente el dashboatf
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    
+    # si usuario envio el formulario post
+    if request.method == 'POST':
+        form = FormularioLogin(request, data = request.POST)
+
+
+#varifica si los datos del formulario son validos
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('dashboard')
+            else:
+                messages.error(request, 'Usuario y Contraseña Incorrectos')
+    else:
+        form = FormularioLogin()
+    return render(request, 'usuarios/login.html', {'form': form})
+#FUNCIONA DE LOGOUT
+@login_required
+def cerrar_sesion(request):
+    logout(request)
+    messages.success(request, 'Cierre de Sesion exitoso')
+    return redirect ('login')
