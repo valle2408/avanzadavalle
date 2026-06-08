d1cbf19ee48047e513a4c87a226f11b08fa9ad34
Valle <vallegarciajuan8@gmail.com>
2026-06-08 13:42:43 -0400

    creamos un formulario admin para django admin de productores para realizar las pruebas productores

--- productores/admin.py
+++ productores/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import Productor
 
-# Register your models here.
+
+# Admin del modelo Productor.
+# Sirve para ver productores desde Django Admin.
+# En el sistema normal, los productores se crearán automáticamente
+# desde Registrar compra de café.
+@admin.register(Productor)
+class ProductorAdmin(admin.ModelAdmin):
+
+    # Columnas que se verán en la lista del admin.
+    list_display = (
+        'codigo_productor',
+        'nombre',
+        'apellido_paterno',
+        'apellido_materno',
+        'comunidad',
+        'estado',
+        'usuario',
+        'fecha_registro',
+    )
+
+    # Campos por los que se podrá buscar en el admin.
+    search_fields = (
+        'codigo_productor',
+        'nombre',
+        'apellido_paterno',
+        'apellido_materno',
+    )
+
+    # Filtros laterales del admin.
+    list_filter = (
+        'estado',
+        'comunidad',
+    )
+
+    # Campos que no deberían editarse manualmente desde admin.
+    readonly_fields = (
+        'fecha_registro',
+        'fecha_edicion',
+    )
+
+    # Orden de visualización.
+    ordering = (
+        'nombre',
+        'apellido_paterno',
+    )
