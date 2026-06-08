2fd6ecd77cb7fc8df706381d21eec10f42866804
Valle <vallegarciajuan8@gmail.com>
2026-06-08 13:19:16 -0400

    creamos la ruta en urls de avanzada para agarrar todo

--- avanzada/urls.py
+++ avanzada/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('',include('principal.urls')), # rutas del dashboard o principal
     path('comunidades/', include('comunidades.urls')), # de la comunidad
     path('', RedirectView.as_view(pattern_name='login', permanent=False)), # para la redirecciona al login directamente
+    path('productores/', include('productores.urls')), # agregamos las acciones de productres
 ]
