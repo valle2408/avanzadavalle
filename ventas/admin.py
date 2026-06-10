2cf8f35e19a4bbe7ef4392690003944787d196be
Valle <vallegarciajuan8@gmail.com>
2026-06-09 20:48:38 -0400

    realizamos el admin para ventas tambien

--- ventas/admin.py
+++ ventas/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import VentaCafe
 
-# Register your models here.
+
+@admin.register(VentaCafe)
+class VentaCafeAdmin(admin.ModelAdmin):
+    list_display = (
+        'numero_ingreso',
+        'fecha_venta',
+        'empresa_compradora',
+        'tipo_cafe',
+        'cantidad_sacos',
+        'peso_total_kg',
+        'registrado_por',
+    )
+
+    search_fields = (
+        'numero_ingreso',
+        'empresa_compradora',
+        'tipo_cafe',
+    )
+
+    list_filter = (
+        'tipo_cafe',
+        'fecha_venta',
+    )
+
+    readonly_fields = (
+        'fecha_registro',
+    )
